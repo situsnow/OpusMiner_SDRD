76a0dd924a231cd42dfbf125ad6450477145f98b
situsnow <situsnow@163.com>
2017-07-18 22:48:57 +1000

    Update with prove of self-sufficient itemset

--- opus/Test.java
+++ opus/Test.java
@@ -1,14 +1,24 @@
 package opus;
 
-import java.util.Date;
 
 public class Test {
 
 	public static void main(String[] args) {
 
-		Date now = new Date();
+		int n = 8416;
+		int count = 3472;
 		
-		System.out.println(now.getTime());
+		int count1 = 4176;
+		
+		int count2 = 3808;
+		
+		int a = n - count1 - count2 + count;
+		int b = count1 - count;
+		int c = count2 - count;
+		int d = count;
+		
+		System.out.println(Fisher.fisherTest(a, b, c, d));
+		
 	}
 
 }
